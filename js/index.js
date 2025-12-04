7ab2ac52d6381cb6ee9c97155a059b3e3e90f6c7
Rafael Valentim <rmoraesvalentim@gmail.com>
2025-12-04 14:51:20 -0300

    Class Chip8 initialized

--- js/index.js
+++ js/index.js
@@ -1,5 +1,15 @@
 const canvas = document.querySelector("#canvas")
-const ctx = canvas.getContext("2d")
 
-ctx.fillStyle = "black";
-ctx.fillRect(0, 0, 640, 320);
+class Chip8 {
+    constructor(canvas) {
+        // Display
+        this.canvas = canvas
+        this.canvas.width = 640
+        this.canvas.height = 320
+        this.context = canvas.getContext("2d")
+        this.context.fillStyle = "black"
+        this.context.fillRect(0, 0, this.canvas.width, this.canvas.height)
+    }
+}
+
+const chip8 = new Chip8(canvas)
