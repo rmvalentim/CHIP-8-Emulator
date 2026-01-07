67294a42e05066873706f7b9c3a34b6d35d73603
Rafael Valentim <rmoraesvalentim@gmail.com>
2026-01-07 16:47:59 -0300

    CPU initialized

--- python/chip8/cpu.py
+++ python/chip8/cpu.py
@@ -1,3 +1,13 @@
 class Cpu:
     def __init__(self):
-        print("CPU Initialized")
+        self.V = bytearray(16)
+        self.I = 0
+        self.pc = 0x200
+        self.stack = []
+        self.sp = 0
+        self.delay_timer = 0
+        self.sound_timer = 0
+        self.display = [0] * (64 * 32)
+        self.keys = [0] * 16
+        self.paused = False
+        self.speed = 3
